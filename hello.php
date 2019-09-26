044d5dd5c83e2b0651616fab57fb5bc511f748b1
Silvano Bakacak <2119823@talnet.nl>
2019-09-26 09:26:22 +0200

    Uitwerking van de exercise

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php
+$tekst = "hallo mensjes\n";
+
+echo $tekst;
+echo $tekst;
+echo $tekst;
+echo $tekst;
+echo $tekst;echo $tekst;
+echo $tekst;echo $tekst;echo $tekst;echo $tekst;echo $tekst;echo $tekst;
+echo $tekst;echo $tekst;echo $tekst;
+echo $tekst;
+
+//onblij
